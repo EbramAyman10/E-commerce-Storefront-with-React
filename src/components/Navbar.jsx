9276e2e93b28b923bc363a90a4370f11d5bffd34
Ebram Ayman <ebramayman55@gmail.com>
2025-11-12 15:28:04 +0200

    updating some css in homepage and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link, NavLink } from "react-router-dom";
 import "./navbar.css";
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-transparent px-3 py-2 mb-4  text-black">
+    <nav className="navbar navbar-expand-lg bg-transparent px-3 py-2 fixed-top  text-black">
       <div className="container-fluid d-flex justify-content-between align-items-center w-100">
         <div className="left-section">
           <Link className="navbar-brand text-black  fs-2 fw-bold" to="/">
