22fd6c026bd6c8ab0f5d7fd75cb03315488ea236
Ebram Ayman <ebramayman55@gmail.com>
2025-11-12 02:16:26 +0200

    finishing the homepage

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,6 +1,12 @@
 import "./HomePage.css";
 export default function HomePage() {
-  return (<>
-
-  </>);
+  return (
+    <div className="home-page">
+      <div className="hero-content">
+        <h2>Unleash Your</h2>
+        <h2>Unique Style</h2>
+        <button className="btn ">Shop Now</button>
+      </div>
+    </div>
+  );
 }
