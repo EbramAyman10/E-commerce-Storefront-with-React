fcbaf9a82c3ef7c7eef47f71631694f0c7eaccc9
Abd-El Rahman Taha <b0ody66taha@gmail.com>
2025-11-14 16:37:34 +0200

    Adding Shop page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
 import Navbar from "./components/Navbar";
+import ShopPage from "./pages/ShopPage";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
       </Routes>
     </>
   );
