2014f189155b94372c37690fc54360f631f0a393
Abd-El Rahman Taha <b0ody66taha@gmail.com>
2025-11-17 01:24:24 +0200

    Adding the login page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,35 +6,33 @@ export default function Navbar() {
     <nav className="navbar navbar-expand-lg bg-transparent px-3 py-2 fixed-top text-black">
       <div className="container-fluid d-flex justify-content-between align-items-center w-100">
 
-      
+        {/* LEFT SECTION */}
         <div className="left-section">
           <Link className="navbar-brand text-black fw-bold" to="/">
             WEARLY
           </Link>
         </div>
 
-        
+        {/* TOGGLER */}
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-      
+        {/* CENTER LINKS */}
         <div className="collapse navbar-collapse" id="navbarNav">
           <div className="middle-section mx-auto">
             <ul className="navbar-nav ms-auto flex-column flex-lg-row gap-4 text-black">
+
               <li className="nav-item">
                 <NavLink
                   to="/shop"
                   className={({ isActive }) =>
-                    isActive ? "nav-link active text-warning" : "nav-link text-black"
+                    isActive ? "nav-link active" : "nav-link"
                   }
                 >
                   Shop
@@ -45,7 +43,7 @@ export default function Navbar() {
                 <NavLink
                   to="/men"
                   className={({ isActive }) =>
-                    isActive ? "nav-link active text-warning" : "nav-link text-black"
+                    isActive ? "nav-link active" : "nav-link"
                   }
                 >
                   Men
@@ -56,7 +54,7 @@ export default function Navbar() {
                 <NavLink
                   to="/women"
                   className={({ isActive }) =>
-                    isActive ? "nav-link active text-warning" : "nav-link text-black"
+                    isActive ? "nav-link active" : "nav-link"
                   }
                 >
                   Women
@@ -67,7 +65,7 @@ export default function Navbar() {
                 <NavLink
                   to="/kids"
                   className={({ isActive }) =>
-                    isActive ? "nav-link active text-warning" : "nav-link text-black"
+                    isActive ? "nav-link active" : "nav-link"
                   }
                 >
                   Kids
@@ -78,30 +76,57 @@ export default function Navbar() {
                 <NavLink
                   to="/collections"
                   className={({ isActive }) =>
-                    isActive ? "nav-link active text-warning" : "nav-link text-black"
+                    isActive ? "nav-link active" : "nav-link"
                   }
                 >
                   Collection
                 </NavLink>
               </li>
+
             </ul>
           </div>
         </div>
 
-        
         <div className="right-section d-flex">
-          <button className="btn icon-button fs-5">
+
+          <NavLink
+            to="/login"
+            className={({ isActive }) =>
+              isActive ? "icon-button active-icon" : "icon-button"
+            }
+          >
             <i className="fa-regular fa-circle-user icon"></i>
-          </button>
-          <button className="btn icon-button fs-5">
+          </NavLink>
+
+          <NavLink
+            to="/search"
+            className={({ isActive }) =>
+              isActive ? "icon-button active-icon" : "icon-button"
+            }
+          >
             <i className="fa-solid fa-magnifying-glass icon"></i>
-          </button>
-          <button className="btn icon-button fs-5">
+          </NavLink>
+
+         
+          <NavLink
+            to="/cart"
+            className={({ isActive }) =>
+              isActive ? "icon-button active-icon" : "icon-button"
+            }
+          >
             <i className="fa-solid fa-cart-shopping icon"></i>
-          </button>
-          <button className="btn icon-button fs-5">
+          </NavLink>
+
+       
+          <NavLink
+            to="/contact"
+            className={({ isActive }) =>
+              isActive ? "icon-button active-icon" : "icon-button"
+            }
+          >
             <i className="fa-solid fa-phone icon"></i>
-          </button>
+          </NavLink>
+
         </div>
 
       </div>
