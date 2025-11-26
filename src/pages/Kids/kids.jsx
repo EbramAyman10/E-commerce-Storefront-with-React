9a0ec514fec47801d057034451ac9b3786ba10f8
Ebram Ayman <ebramayman55@gmail.com>
2025-11-26 19:43:55 +0200

    adding navbar2 to all sections

--- src/pages/Kids/kids.jsx
+++ src/pages/Kids/kids.jsx
@@ -1,15 +1,30 @@
 import { useProducts } from "../../context/ProductContext";
 import ProductCard from "../../components/productCard";
+import Navbar2 from "../../components/Navbar2";
 
 export default function Kids() {
-  const { products } = useProducts();
-
-  const kidsProducts = products.filter(
-    (item) => item.category === "men's clothing"
+  const { filteredProducts, selectedProduct, setSelectedProduct } =
+    useProducts();
+  const kidsProducts = filteredProducts.filter(
+    (item) => item.category === "kids' clothing"
   );
   return (
     <>
       <div className="container mt-5">
+        <div className="top-section ">
+          <div className="left-title">
+            <h3 className="mb-2 fs-2" id="top">
+              Kids' Products
+            </h3>
+          </div>
+
+          <div className="right-navbar">
+            <Navbar2
+              onselectProduct={setSelectedProduct}
+              selectedProduct={selectedProduct}
+            />
+          </div>
+        </div>
         <div className="row g-4">
           {kidsProducts.map((product) => (
             <ProductCard key={product.id} product={product} />
--- src/pages/Women/women.jsx
+++ src/pages/Women/women.jsx
@@ -1,15 +1,30 @@
 import { useProducts } from "../../context/ProductContext";
 import ProductCard from "../../components/productCard";
+import Navbar2 from "../../components/Navbar2";
 
 export default function Women() {
-  const { products } = useProducts();
-
-  const womenProducts = products.filter(
+  const { filteredProducts, selectedProduct, setSelectedProduct } =
+    useProducts();
+  const womenProducts = filteredProducts.filter(
     (item) => item.category === "women's clothing"
   );
   return (
     <>
       <div className="container mt-5">
+        <div className="top-section ">
+          <div className="left-title">
+            <h3 className="mb-2 fs-2" id="top">
+              Women's Products
+            </h3>
+          </div>
+
+          <div className="right-navbar">
+            <Navbar2
+              onselectProduct={setSelectedProduct}
+              selectedProduct={selectedProduct}
+            />
+          </div>
+        </div>
         <div className="row g-4">
           {womenProducts.map((product) => (
             <ProductCard key={product.id} product={product} />
